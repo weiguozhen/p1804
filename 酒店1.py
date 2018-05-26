98105abb6ee735afd3a6afc02366d6c04407b88f
weiguozhen <1527183103@qq.com>
2018-05-26 17:37:14 +0800

    酒店1

---
+++ 酒店1.py
@@ -0,0 +1,65 @@
+import 酒店
+
+酒店.huanying()
+while True:
+    m=input("请输入选择的服务:")
+    if m=="1":
+        酒店.luru()
+    elif m=="2":
+        酒店.all1()
+    elif m=="3":
+        酒店.zhiding()
+    elif m=="4":
+        酒店.xiugai()
+    elif m=="5":
+        酒店.del1()
+    elif m=="6":
+        酒店.jie()
+    elif m=='7':
+        酒店.huiyuan()
+    elif m=='8':
+        酒店.cccc()
+    elif m=='9':
+        酒店.tuichu()
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
